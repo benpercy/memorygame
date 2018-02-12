32f7e319e135db245bce786babc8503ae0431f37
benpercy <up773455@myport.ac.uk>
2018-02-12 20:42:35 +0000

    Added random square generation

--- files/sketch.js
+++ files/sketch.js
@@ -1,6 +1,7 @@
 var counter = 0;
 var timeLeft = 5;
 var squares = [];
+var newSquare;
 
 function convertSeconds(s){
   var min = floor (s/60);
@@ -10,29 +11,40 @@ function convertSeconds(s){
 
 function setup() {
   createCanvas(600, 400);
-  for (i=0 ; i<10; i++){
-    squares[i] = new Square();
+  background(51);
+  squares.push(new Square());
+  console.log(squares);
+  for (i=0; i<10; i++){
+    newSquare = new Square();
+    console.log(newSquare.colour);
+    console.log(squares);
+    if(squares[i].colour == newSquare.colour){
+      console.log("Duplicate!");
+        continue;
+    }else{
+      squares[i] = newSquare;
+    }
   }
 
 
+  //timer handling
   var timer = select('#timer');
   timer.html("Time Remaining: " + convertSeconds(timeLeft - counter ));
-
   function timeIt(){
     if(timeLeft - counter <= 0){
       timer.html("Times up!");
     }else{
     counter++;
     timer.html("Time Remaining: " + convertSeconds(timeLeft - counter ));
+    }
   }
-}
 
   setInterval(timeIt, 1000);
-}
 
+//draw all objects on the page
 function draw(){
-  background(51);
   for (i=0; i<squares.length;i++){
     squares[i].show();
   }
+}
 }
--- files/square.js
+++ files/square.js
@@ -1,12 +1,15 @@
-var colours = ["white","silver","gray","black","red","maroon", "yellow", "olvie", "lime","green","aqua","teal","blue","navy","fuchsia","purple"]
+var colours = ["white","silver","gray","black","red","maroon", "yellow", "olive", "lime","green","aqua","teal","blue","navy","fuchsia","purple"]
 
 function Square(){
   var rand = colours[Math.floor(Math.random() * colours.length)];
+  this.colour = rand;
   this.x = Math.floor(Math.random() * (570 - 0 + 1));
   this.y = Math.floor(Math.random() * (370 - 0 + 1));
 
+  newRec = rect(this.x, this.y, 30, 30);
+  newRec.fill(this.colour);
+
   this.show = function(){
-    fill(rand);
-    rect(this.x, this.y, 30, 30);
+    newRec.show();
   }
 }
